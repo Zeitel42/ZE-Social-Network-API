6610e4c49cef0167a0396dced7d2ae7a1202b749
Zeitel42 <43343991+Zeitel42@users.noreply.github.com>
2022-06-10 14:25:27 -0400

    added User model

---
+++ models/User.js
@@ -0,0 +1,33 @@
+const { Schema, model } = require("mongoose");
+
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
+  },
+
+  thoughts: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Thought",
+    },
+  ],
+  friends: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+});
+
+const User = model("User", UserSchema);
+
+module.exports = User;
