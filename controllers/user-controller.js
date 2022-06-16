c1c1db6ec91f8a4f8fef35f57d5ff67480c0f70b
Zeitel42 <43343991+Zeitel42@users.noreply.github.com>
2022-06-16 06:39:40 -0400

    added add/remove friend post/delete routes tested good

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -32,7 +32,7 @@ const userController = {
       });
   },
 
-  // createUser
+  // create User
   createUser({ body }, res) {
     User.create(body)
       .then((dbUserData) => res.json(dbUserData))
@@ -59,6 +59,37 @@ const userController = {
   deleteUser({ params }, res) {
     User.findOneAndDelete({ _id: params.id })
       .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => res.json(err));
+  },
+  // add Friend
+  addFriend({ params }, res) {
+    User.findOneAndUpdate(
+      { _id: params.userId },
+      { $addToSet: { friends: params.friendId } },
+      { new: true, runValidators: true }
+    )
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "No user with this id" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => res.json(err));
+  },
+  //delete friend
+  removeFriend({ params }, res) {
+    User.findOneAndUpdate(
+      { _id: params.userId },
+      { $pull: { friends: params.friendId } },
+      { new: true }
+    )
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          return res.status(404).json({ message: "No user with this id!" });
+        }
+        res.json(dbUserData);
+      })
       .catch((err) => res.json(err));
   },
 };
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -5,13 +5,18 @@ const {
   createUser,
   updateUser,
   deleteUser,
+  addFriend,
+  removeFriend,
 } = require("../../controllers/user-controller");
 const User = require("../../models");
 
-// /api/Users
+// /api/users
 user.route("/").get(getAllUser).post(createUser);
 
-// /api/Users/:id
+// /api/users/:id
 user.route("/:id").get(getUserById).put(updateUser).delete(deleteUser);
+
+// /api/users/:userId/friends/:friendId
+user.route("/:userId/friends/:friendId").post(addFriend).delete(removeFriend);
 
 module.exports = user;
