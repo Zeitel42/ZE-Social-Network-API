0be8b134a74bb4785406f708a79ad41be4eb7ad7
Zeitel42 <43343991+Zeitel42@users.noreply.github.com>
2022-06-17 09:28:08 -0400

    merge conflict resolved

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -54,7 +54,11 @@ const thoughtController = {
       })
       .then((dbUserData) => {
         if (!dbUserData) {
-          res.status(404).json({ message: "No user found with this id!" });
+          res
+            .status(404)
+            .json({
+              message: "Ghost thought created! No user found with this id.",
+            });
           return;
         }
         res.json(dbUserData);
